633cc7c00cf58bde67681e6e6692662313302ce5
Yash-scaletech <yash.upadhyay@scaletech.xyz>
2023-06-14 18:35:37 +0530

    start dashboard style

--- src/features/dashboard/component/dashboardData.tsx
+++ src/features/dashboard/component/dashboardData.tsx
@@ -1,5 +1,12 @@
 const DashboardData = () => {
-	return <div>D3</div>;
+	return (
+		<div className='ml--20'>
+			<div>
+				<h1>Hello Yash Upadhyay,</h1>
+				<p>Have a nice day and don't forget to take care of your health!</p>
+			</div>
+		</div>
+	);
 };
 
 export default DashboardData;
