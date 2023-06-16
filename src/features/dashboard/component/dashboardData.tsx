417133de859cb6a043e083bb4c4d6c614bcc9637
Yash-scaletech <yash.upadhyay@scaletech.xyz>
2023-06-16 09:40:30 +0530

    activity data added

--- src/features/dashboard/component/dashboardData.tsx
+++ src/features/dashboard/component/dashboardData.tsx
@@ -1,18 +1,58 @@
 import yoga from '../../../assets/images/yoga.png';
+import walking from '../../../assets/images/walking.png';
+import burn from '../../../assets/images/burn.png';
+import water from '../../../assets/images/water.png';
 
 const DashboardData = () => {
 	return (
-		<div className='name-wrapper flex align-items--center width--full mt--20'>
-			<div className='width--70 p--10 ml--25'>
-				<h1 className='line-height--70 font--medium'>
-					<span className='font-size--48 text--secondary'>Hello</span> Yash Upadhyay,
-				</h1>
-				<p className='font-size--md line-height--25 font--regular'>
-					Have a nice day and don't forget to take <br /> care of your health!
-				</p>
+		<div>
+			<div className='name-wrapper flex align-items--center width--full mt--20'>
+				<div className='width--70 p--10 ml--25'>
+					<h1 className='line-height--70 font--medium'>
+						<span className='font-size--48 text--secondary'>Hello</span> Yash Upadhyay,
+					</h1>
+					<p className='font-size--xxl line-height--25 font--regular'>
+						Have a nice day and don't forget to take <br /> care of your health!
+					</p>
+				</div>
+				<div className='width--30 mr--15'>
+					<img src={yoga} alt='profile' className='width--full yoga-image' />
+				</div>
 			</div>
-			<div className='width--30 mr--15'>
-				<img src={yoga} alt='profile' className='width--full yoga-image' />
+			<div className='mt--25'>
+				<div className='flex width--full justify-content--evenly'>
+					<div className='flex justify-content--center width--33 activity-box mr--30'>
+						<div className='image-wrapper flex align-items--center justify-content--center'>
+							<img src={walking} alt='walking' />
+						</div>
+						<div>
+							<p className='font-size--26 line-height--50 font--semi-bold'>1000/1500</p>
+							<p className='font-size--22 line-height--30 font--regular'>steps taken</p>
+						</div>
+					</div>
+					<div className='flex justify-content--center width--33 activity-box mr--30'>
+						<div className='image-wrapper flex align-items--center justify-content--center'>
+							<img src={burn} alt='burn' className='burn-image' />
+						</div>
+						<div>
+							<p className='font-size--26 line-height--50 font--semi-bold'>
+								500 <span>kcal</span>
+							</p>
+							<p className='font-size--22 line-height--30 font--regular'>Calories burned</p>
+						</div>
+					</div>
+					<div className='flex justify-content--center width--33 activity-box'>
+						<div className='image-wrapper flex align-items--center justify-content--center'>
+							<img src={water} alt='water' />
+						</div>
+						<div>
+							<p className='font-size--26 line-height--50 font--semi-bold'>
+								30 <span>liters</span>
+							</p>
+							<p className='font-size--22 line-height--30 font--regular'>water taken</p>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
